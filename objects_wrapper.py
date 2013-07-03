7723831e11690936cad442389a4b5db26c62677e
filareta <filareta_yordanova@abv.bg>
2013-07-04 00:12:41 +0300

    Wrap game objects

---
+++ objects_wrapper.py
@@ -0,0 +1,128 @@
+from pygame import image, display, key, transform
+from pygame import K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE, K_TAB, K_w, K_s, K_a, K_d
+from pygame.sprite import Sprite, collide_rect
+from pygame.rect import Rect
+
+from vector import Vec2D, to_pixels, to_coords
+from settings import ENEMIES, TILE_SIZE, SIZE_X, SIZE_Y
+
+
+class PlayerWrapper(Sprite):
+    bullet = None
+
+    def __init__(self, player):
+        super(PlayerWrapper, self).__init__()
+        self.player = player
+        self.convert(to_pixels)
+        self.image = image.load("assets/player{}.png".format(player.turn))
+        self.rect = Rect((player.coords[0][0], player.coords[0][1]), self.image.get_size())
+
+    def convert(self, function):
+        self.player.coords = [function(tile) for tile in self.player.coords]
+
+    def shoot(self):
+        self.bullet = BulletSprite(self.player.create_bullet())
+
+    def update(self, alpha, world, walls):
+        if not self.player.dead:
+            choice = key.get_pressed()
+            if self.player.turn == 1:
+                control = (K_UP, K_DOWN, K_LEFT, K_RIGHT, K_SPACE)
+                sign = 'Y'
+            else:
+                control = (K_w, K_s, K_a, K_d, K_TAB)
+                sign = 'G'
+            world.clear_content(self.player.coords[0], sign)
+            direction = Vec2D(0, 0)
+            if choice[control[0]]:
+                direction = Vec2D(0, -1)
+                self.player.angle = 0
+            if choice[control[1]]:
+                direction = Vec2D(0, 1)
+                self.player.angle = -180
+            if choice[control[2]]:
+                direction = Vec2D(-1, 0)
+                self.player.angle = 90
+            if choice[control[3]]:
+                direction = Vec2D(1, 0)
+                self.player.angle = -90
+            x, y = direction * TILE_SIZE
+            self.rect.left += x
+            self.rect.top += y
+            for wall in walls:
+                if self.rect.colliderect(wall):
+                    direction = Vec2D(0, 0)
+                    self.rect.left -= x
+                    self.rect.top -= y
+            self.player.move(direction, world.world)
+            if choice[control[4]]:
+                self.shoot()
+            world.set_content(sign, self.player.coords)
+
+    def draw(self, screen):
+        if not self.player.dead:
+            origin = self.image.convert_alpha()
+            rotated = transform.rotate(origin, self.player.angle)
+            screen.blit(rotated, (self.player.coords[0][0], self.player.coords[0][1]))
+
+
+class EnemyWrapper(Sprite):
+    def __init__(self, enemy, pic):
+        super(EnemyWrapper, self).__init__()
+        self.enemy = enemy
+        self.enemy.turn = pic
+        self.convert(to_pixels)
+        self.image = image.load("assets/" + ENEMIES[pic])
+        self.rect = Rect((self.enemy.coords[0][0], self.enemy.coords[0][1]), self.image.get_size())
+
+    def convert(self, function):
+        self.enemy.coords = [function(tile) for tile in self.enemy.coords]
+
+    def bullet(self):
+        if self.enemy.bullet:
+            return BulletSprite(self.enemy.bullet)
+
+    def update(self, direction, alpha, world, index, cls):
+        if self.enemy.direction.zero():
+            self.enemy.direction = -direction
+        cls.clear_content(self.enemy.coords[0], 'E')
+        self.enemy.find_neighbours(self.enemy.coords[0], index, world)
+        self.enemy.move(world, alpha)
+        cls.set_content('E', self.enemy.coords)
+        x, y = self.enemy.direction * TILE_SIZE
+        self.rect.left += x
+        self.rect.top += y
+
+    def draw(self, screen):
+        origin = self.image.convert_alpha()
+        rotated = transform.rotate(origin, self.enemy.angle)
+        screen.blit(rotated, (self.enemy.coords[0][0], self.enemy.coords[0][1]))
+
+
+class BulletSprite(Sprite):
+    active = True
+
+    def __init__(self, bullet):
+        super(BulletSprite, self).__init__()
+        self.bullet = bullet
+        self.image = image.load("assets/bullet.png")
+        x, y = to_pixels(self.bullet.pos)
+        self.rect = Rect((x, y), self.image.get_size())
+
+    def update(self, world, alpha):
+        if self.active:
+            self.bullet.flight(world, alpha)
+            x, y = self.bullet.direction * self.bullet.ttl * alpha
+            self.rect.left += x
+            self.rect.top += y
+
+    def draw(self, screen):
+        if self.active:
+            origin = self.image.convert_alpha()
+            rotated = transform.rotate(origin, self.bullet.angle)
+            screen.blit(rotated, (self.rect.left, self.rect.top))
+
+
+
+
+
