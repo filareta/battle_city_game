c2b92f2a56858319f1dfa3a3f22b8cce3f392e4a
filareta <filareta_yordanova@abv.bg>
2013-07-04 00:12:25 +0300

    Settings file

---
+++ settings.py
@@ -0,0 +1,7 @@
+SIZE_X = 100
+SIZE_Y = 72
+TILE_SIZE = 10
+SCREEN_SIZE = (1000, 720)
+MAPS = {1: "assets/simple_map.txt"}
+ENEMIES = {0: "enemy1.png", 1: "enemy2.png"}
+CONTENT = {'Y': 'Y', 'G': 'G', 'B': 'B', 'W': 'W', 'F': 'F', '0': '0', 'E': 'E', '#': '#'}
