dbb5d8b3526cbaec7fa8bd38d02fadff5528c72b
filareta <filareta_yordanova@abv.bg>
2013-07-04 04:30:10 +0300

    Add test for world creation

--- tests/test_world.py
+++ tests/test_world.py
@@ -43,6 +43,77 @@ class TestWorld(unittest.TestCase):
         self.assertIn(Vec2D(0, 65), self.world.bounds)
         self.assertNotIn(Vec2D(1, 65), self.world.bounds)
 
+    def test_set_wall_energy(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_energy('B', coords)
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].energy, -15)
+
+    def test_set_player_energy(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_energy('Y', coords)
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].energy, 5)
+
+    def test_set_enemy_energy(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_energy('E', coords)
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].energy, -5)
+
+    def test_set_content(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_content('E', coords)
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].content, 'E')
+
+    def test_clear_player(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_content('Y', coords)
+        self.world.set_energy('Y', coords)
+        self.world.clear_content(Vec2D(12, 64), 'Y')
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].content, '0')
+            self.assertEqual(self.world.world[x][y].energy, 5)
+
+    def test_clear_enemy_with_energy(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_content('E', coords)
+        self.world.set_energy('E', coords)
+        self.world.clear_content(Vec2D(12, 64), 'E')
+        for x, y in coords:
+            self.assertEqual(self.world.world[x][y].content, '0')
+            self.assertEqual(self.world.world[x][y].energy, 0)
+
+    def test_in_range(self):
+        self.assertTrue(self.world.in_range(45, 71))
+        self.assertFalse(self.world.in_range(45, 73))
+        self.assertFalse(self.world.in_range(101, 71))
+        self.assertFalse(self.world.in_range(-1, 71))
+
+    def test_neighbours(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_energy('E', coords)
+        filtered = self.world.neighbours(Vec2D(11, 64))
+        answer = [Vec2D(10, 64), Vec2D(11, 63), Vec2D(11, 65)]
+        for cell in filtered:
+            self.assertIn(cell, answer)
+
+    def test_spread_energy(self):
+        coords = [Vec2D(12, 64), Vec2D(13, 64), Vec2D(12, 65), Vec2D(13, 65)]
+        self.world.set_energy('Y', coords)
+        self.world.spread(3, Vec2D(12, 64))
+        self.assertEqual(self.world.world[11][64].energy, 6)
+        self.assertEqual(self.world.world[13][64].energy, 5)
+        self.assertEqual(self.world.world[10][64].energy, 7)
+        self.assertEqual(self.world.world[11][63].energy, 7)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
