842e21c8bc447654e6cf1784123cb1d53931d9a4
filareta <filareta_yordanova@abv.bg>
2013-07-06 04:14:19 +0300

    Fix enemy direction

--- enemy.py
+++ enemy.py
@@ -9,7 +9,6 @@ from settings import TILE_SIZE, ENEMIES, SIZE_X, SIZE_Y
 
 
 class Enemy():
-    SPEED = 20
     coords = []
     direction = Vec2D(0, 0)
     angle = 0
@@ -23,15 +22,15 @@ class Enemy():
         self.coords = coords
 
     def create_bullet(self):
-        self.bullet = Bullet(self.coords[2] + self.direction, self.direction, self.angle, "enemy")
+        start = self.coords[2] + self.direction
+        self.bullet = Bullet(start, self.direction, self.angle, "enemy")
 
     def check_health(self):
+        self.health -= 30
         if self.health <= 0:
             self.alive = False
-        else:
-            self.health -= 10
 
-    def move(self, world, alpha):
+    def move(self, world):
         for i, position in enumerate(self.coords):
             ind1, ind2 = self.coords[i]
             move = position + self.direction
@@ -47,40 +46,40 @@ class Enemy():
             self.angle = 90
 
     def valid_moves(self, cell):
-        directions = [Vec2D(1, 0), Vec2D(0, -1), Vec2D(-1, 0), Vec2D(0, 1)]
+        directions = [Vec2D(4, 0), Vec2D(0, -4), Vec2D(-4, 0), Vec2D(0, 4)]
         cells = [(direction + cell, direction) for direction in directions]
-        check = lambda c: c[0][0] >= 0 and c[0][0] < SIZE_X \
-                          and c[0][1] >= 0 and c[0][1] < SIZE_Y
+        check = lambda c: c[0][0] >= 0 and c[0][0] < SIZE_X and \
+            c[0][1] >= 0 and c[0][1] < SIZE_Y
         return filter(check, cells)
 
     def check_by_cell(self, direction, world):
-        check = lambda tile: self.check_direction(tile, direction, world)
-        return all(map(check, self.coords))
+        return all(map(lambda tile:
+                   self.check_direction(tile, direction, world),
+                   self.coords))
 
     def find_neighbours(self, cell, index, world):
         valid = self.valid_moves(cell)
         neigh_dict = defaultdict(list)
-        for n, direct in valid:
-            if self.check_by_cell(direct, world):
-                key = world[n[0]][n[1]].energy
-                neigh_dict[key].append(direct)
+        for next, direction in valid:
+            if self.check_by_cell(direction // 4, world):
+                key = world[next[0]][next[1]].energy
+                neigh_dict[key].append(direction // 4)
         self.direction = self.find_next(neigh_dict, index, world)
 
     def check_direction(self, cell, direction, world):
         p = cell + direction
-        valid = p[0] >= 0 and p[0] < SIZE_X and p[1] >= 0 and p[1] < SIZE_Y and \
-                (world[p[0]][p[1]].empty() or world[p[0]][p[1]].content == 'Y' or \
-                 world[p[0]][p[1]].content == 'G')
-        return valid
+        return \
+            p[0] >= 0 and p[0] < SIZE_X and \
+            p[1] >= 0 and p[1] < SIZE_Y and \
+            (world[p[0]][p[1]].empty() or
+             world[p[0]][p[1]].content == 'Y' or
+             world[p[0]][p[1]].content == 'G')
 
-    def collision(self, direction, world):
-        cells = [cell + direction for cell in self.coords if self.check_direction(cell, direction, world)]
-        return all([world[x][y].content == 'Y' or world[x][y].content == 'G' for x, y in cells])
-
-
-    def partial_collision(self, direction, world):
-        cells = [cell + direction for cell in self.coords if self.check_direction(cell, direction, world)]
-        return any([world[x][y].content == 'Y' or world[x][y].content == 'G' for x, y in cells])
+    def detect_collision(self, direction, world, function):
+        cells = [cell + direction for cell in self.coords
+                 if self.check_direction(cell, direction, world)]
+        return function([world[x][y].content == 'Y' or
+                         world[x][y].content == 'G' for x, y in cells])
 
     def find_next(self, neighbour_dirs, index, world):
         keys = neighbour_dirs.keys()
@@ -89,7 +88,7 @@ class Enemy():
             max_energy = max(keys)
             valid_dirs = neighbour_dirs[max_energy]
             for direction in valid_dirs:
-                if self.partial_collision(direction, world):
+                if self.detect_collision(direction, world, any):
                     self.create_bullet()
             for direction in valid_dirs:
                 if self.direction.same_direction(direction):
@@ -98,23 +97,10 @@ class Enemy():
                 if self.direction.same_direction(-direction):
                     return direction
             if index < len(valid_dirs):
+                self.create_bullet()
                 return valid_dirs[index]
             elif valid_dirs:
                 return valid_dirs[len(valid_dirs)-self.turn-1]
         else:
-            self.roam(world)
-
-    def roam(self, world):
-        if self.check_direction(self.coords[0], self.direction, world) and \
-            self.check_direction(self.coords[-1], self.direction, world):
-            return self.direction
-        elif self.check_direction(self.coords[0], self.direction.rotate(), world) and \
-            self.check_direction(self.coords[-1], self.direction.rotate(), world):
+            self.create_bullet()
             return self.direction.rotate()
-        elif self.check_direction(self.coords[0], -self.direction.rotate(), world) and \
-            self.check_direction(self.coords[-1], -self.direction.rotate(), world):
-            return -self.direction.rotate()
-        elif self.check_direction(self.coords[0], -self.direction, world) and \
-            self.check_direction(self.coords[-1], -self.direction, world):
-            return -self.direction
-
