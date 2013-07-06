3d75dc7f2e092e085be509235dee26439171dbcb
filareta <filareta_yordanova@abv.bg>
2013-07-06 04:16:29 +0300

    Run all tests together

---
+++ run_tests.py
@@ -0,0 +1,7 @@
+import unittest
+from tests.test_vec2d import TestVec2D, TestConvertFunctions
+from tests.test_world import TestWorld
+from tests.test_enemy import TestEnemy
+from tests.test_player import TestPlayer
+
+unittest.main()
