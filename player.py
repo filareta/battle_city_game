da9146cd5ed5e127796ca5eafdbdfbcd9fa69174
filareta <filareta_yordanova@abv.bg>
2013-07-06 04:13:09 +0300

    Fix identation

--- player.py
+++ player.py
@@ -11,7 +11,6 @@ from static_objects import Bullet
 class Player(Sprite):
     coords = []
     direction = Vec2D(0, 0)
-    last_dir = Vec2D(0, -1)
     angle = 0
     health = 100
     dead = False
@@ -22,30 +21,41 @@ class Player(Sprite):
 
     def valid_direction(self, direction, world):
         return self.valid(self.coords[0] + direction, world) and \
-               self.valid(self.coords[-1] + direction, world)
+            self.valid(self.coords[-1] + direction, world)
 
     def valid(self, cell, world):
-        return cell[0] >= 0 and cell[0] < SIZE_X and cell[1] >= 0 and cell[1] < SIZE_Y and \
-               world[cell[0]][cell[1]].empty()
+        return cell[0] >= 0 and cell[0] < SIZE_X and \
+            cell[1] >= 0 and cell[1] < SIZE_Y and \
+            world[cell[0]][cell[1]].passable()
 
     def move(self, direction, world):
         if not self.dead:
             if self.valid_direction(direction, world):
                 self.direction = direction
-                self.last_dir = direction
                 for i, position in enumerate(self.coords):
                     ind1, ind2 = self.coords[i]
                     move = position + direction
                     world[move[0]][move[1]].energy += world[ind1][ind2].energy
                     self.coords[i] = move
+            else:
+                self.direction = Vec2D(0, 0)
 
     def create_bullet(self):
-        if self.direction.zero():
-            return Bullet(self.coords[2] + self.last_dir * 4, self.last_dir, self.angle, "player")
-        else:
-            return Bullet(self.coords[2] + self.direction * 4, self.direction, self.angle, "player")
+        if self.angle == 0:
+            start = self.coords[2] + Vec2D(0, -1)
+            return Bullet(start, Vec2D(0, -1), self.angle, "player")
+        elif self.angle == -180:
+            start = self.coords[2] + Vec2D(0, 1)
+            return Bullet(start, Vec2D(0, 1), self.angle, "player")
+        elif self.angle == 90:
+            start = self.coords[2] + Vec2D(-1, 2)
+            return Bullet(start, Vec2D(-1, 0), self.angle, "player")
+        elif self.angle == -90:
+            start = self.coords[2] + Vec2D(1, 2)
+            return Bullet(start, Vec2D(1, 0), self.angle, "player")
 
     def check_health(self, decrease):
         self.health -= decrease
         if self.health <= 0:
             self.dead = True
+
